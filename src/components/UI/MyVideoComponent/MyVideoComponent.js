c6016e01cd9f47192d31d2925b2e6584333833d4
Joshua Smith <kazenostigma50@gmail.com>
2024-03-05 17:46:29 -0600

    removed controls on videos for projects section

--- src/components/UI/MyVideoComponent/MyVideoComponent.js
+++ src/components/UI/MyVideoComponent/MyVideoComponent.js
@@ -1,32 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Suspense } from "react";
 const LazyReactPlayer = React.lazy(() => import("react-player"));
 
 const MyVideoComponent = ({ videoSrc, width, height }) => {
-    const [showControls, setShowControls] = useState(false);
-
   return (
-    <div
-    onMouseEnter={() => setShowControls(true)}
-    onMouseLeave={() => setShowControls(false)}
-    >
+    <div>
       <Suspense fallback={<div>Loading...</div>}>
-      <LazyReactPlayer
-        url={videoSrc}
-        width={width}
-        height={height}
-        playing
-        muted={true}
-        loop
-        controls={showControls}
-        config={{
+        <LazyReactPlayer
+          url={videoSrc}
+          width={width}
+          height={height}
+          playing
+          muted={true}
+          loop
+          config={{
             youtube: {
-              playerVars: { 'vq': 'hd1080' } 
-            }
+              playerVars: { vq: "hd1080" },
+            },
           }}
-      />
+        />
       </Suspense>
-    
     </div>
   );
 };
